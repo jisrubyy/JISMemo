383b7bcb4be58d59880f4c83d8afb771b7e7622e
teamjistory <teamjistory@gmail.com>
2025-11-06 15:57:28 +0900

    ✨ docs(AppInfo.cs): Description 문장 끝에 마침표 추가

--- AppInfo.cs
+++ AppInfo.cs
@@ -6,7 +6,7 @@ public static class AppInfo
     public static string FullVersion => $"{Version}.{DateTime.Now:yyyyMMdd}";
     public const string AppName = "JISMemo";
     public const string Developer = "Jisrubyy";
-    public const string Description = "포스트잇 스타일 메모 애플리케이션";
+    public const string Description = "포스트잇 스타일 메모 애플리케이션.";
     public const string ContactEmail1 = "jisrubyy@gmail.com";
     public const string ContactEmail2 = "zegtern@kakao.com";
     public static string ContactEmails => $"{ContactEmail1}, {ContactEmail2}";
